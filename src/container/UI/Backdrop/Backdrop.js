6ae3e5b2c6e95313b7f6af0d72216e7b39c461d9
kavyatripathi <48882133+kavyatripathi@users.noreply.github.com>
2020-05-04 23:38:39 -0700

    Delete Backdrop.js

--- src/container/UI/Backdrop/Backdrop.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Backdrop from '../../../assets/shinchan.jpg';
-
-const backdrop = (props) => {
-    return <img src={Backdrop} alt="Backdrop" style={{height: props.height}}/>;
-}
-
-export default backdrop;
