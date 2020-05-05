0fe11d6b96076592f5a713c7292120ed64ede435
kavyatripathi <48882133+kavyatripathi@users.noreply.github.com>
2020-05-04 23:40:56 -0700

    Update App.js
    Removed backdrop folder because it was unecessary

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import FormDetails from './components/FormDetails/FormDetails';
-import Backdrop from './container/UI/Backdrop/Backdrop';
 class App extends Component {
   render() {
     return (
